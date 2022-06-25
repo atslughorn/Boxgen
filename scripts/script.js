9ea7a56879016ba5275fc62c9981f58baefb8425
Charlie Thomas <charliepthomas@live.co.uk>
2022-06-25 17:55:11 +0100

    added input validation

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,14 @@ let noOfBoxes = document.getElementById("noOfBoxes")
 noOfBoxes.addEventListener("input", generateBoxes)
 
 function generateBoxes(e){
+    let number = parseInt(e.target.value)
+    if (number > 1000 || number < 0) {
+        number = 0
+        e.target.value = number
+    }
+
     boxes.innerHTML = ""
-    for (let i=0;i<e.target.value;i++){
+    for (let i=0;i<number;i++){
         let box=document.createElement("div")
         boxes.appendChild(box)
         box.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
